d725711362bce68457e6d948f7ee88b00c7b4034
shubham30102002 <shubhamrg30@gmail.com>
2023-12-21 01:19:28 +0530

    button tab

---
+++ src/components/ThemeProvider.js
@@ -0,0 +1,20 @@
+// ContextProvider.js
+import React, { createContext, useState } from 'react';
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const theme = isDarkMode ? 'dark' : 'light';
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
