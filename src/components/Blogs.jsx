2380285dc42e130bf8197fd42abe1edb2aa46acb
shubham30102002 <shubhamrg30@gmail.com>
2023-12-22 00:34:23 +0530

    routes

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,47 +1,25 @@
-import React, { useContext } from 'react'
-import { AppContext } from '../context/AppContext'
-import Spinner from './Spinner';
-import "./Blogs.css"
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import BlogDetails from "./BlogDetails";
 
-const Blogs = () => {
-    //consume
-    const {posts,loading} = useContext(AppContext);
-    console.log("Printing inside blogs component");
-    console.log(posts);
-
+export default function Blogs() {
+  const { posts, loading } = useContext(AppContext);
 
   return (
-    <div className='w-11/12 max-w-[560px] h-screen py-8 flex flex-col gap-y-7 mt-[360px] mb-[360px]  justify-center items-center'>
-    {
-        loading ? 
-
-        (<Spinner />) : 
-
-        (   
-            posts.length === 0 ? 
-            (<div>
-                <p>No Post Found</p>
-            </div>) : 
-            (posts.map( (post) => (
-                <div key={post.id}>
-                    <p className="font-bold text-lg ">{post.title}</p>
-                    <p className='text-sm mt-[4px]'>
-                        By <span className='italic'>{post.author}</span> on <span className='underline font-bold'>{post.category} </span>
-                    </p>
-                    <p className='text-sm mt-[4px]'>Posted on {post.date}</p>
-                    <p className='text-md mt-[14px]'>{post.content}</p>
-                    <div className='flex gap-x-3'>
-                        {post.tags.map( (tag, index) => {
-                            return <span key={index} className="text-blue-700 underline font-bold text-xs mt-[5px]">{` #${tag}`}</span>
-                        } )}
-                    </div>
-                </div>
-            ) ))
-        ) 
-    }
-      
+    <div className="flex flex-col gap-y-10 my-4">
+      {loading ? (
+        <div className="min-h-[80vh] w-full flex justify-center items-center">
+          <p className="text-center font-bold text-3xl">Loading</p>
+        </div>
+      ) : posts.length === 0 ? (
+        <div className="min-h-[80vh] w-full flex justify-center items-center">
+          <p className="text-center font-bold text-3xl">No Blogs Found !</p>
+        </div>
+      ) : (
+        posts.map((post) => (
+          <BlogDetails key={post.id} post={post}/>
+        ))
+      )}
     </div>
-  )
+  );
 }
-
-export default Blogs
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,9 @@
-import React from 'react'
-
-const Header = () => {
+export default function Header() {
   return (
-    <div className='w-full border shadow-md py-4 fixed top-0 bg-white '>
-      <header className='text-center' >
-        <h1 className='text-3xl font-bold uppercase'>Codehelp Blogs</h1>
-      </header>
-    </div>
-  )
+    <header className="py-4 border-b-2 border-b-gray-300 drop-shadow-md fixed top-0 inset-x-0 bg-white">
+      <h1 className="font-bold text-3xl uppercase text-center">
+        Codehelp Blogs
+      </h1>
+    </header>
+  );
 }
-
-export default Header
